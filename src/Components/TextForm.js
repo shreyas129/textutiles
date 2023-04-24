65c694a8e3de2524dbb1878dd4409f34cca897dd
shreyas129 <shreyas.tathode129@gmail.com>
2023-04-24 15:29:44 +0530

    too many changes

---
+++ src/Components/TextForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked: " + text);
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLoClick = () => {
+        // console.log("Uppercase was clicked: " + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = () => {
+        // console.log("Uppercase was clicked: " + text);
+        let newText = '';
+        setText(newText)
+    }
+
+    const handleCopy = () => {
+        // console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        // text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState('');
+    // text = "new text"; // Wrong way to change text
+    // setText("new text"); // Correct way to change text
+    return (
+        <>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea class="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: {props.mode === 'light' ? 'dark' : 'light' }}} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy text</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra spaces</button>
+            </div>
+            <div className="container my-3">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
