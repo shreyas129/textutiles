4f81f5142006595d26285166e2b110c5e27706ff
shreyas129 <shreyas.tathode129@gmail.com>
2023-04-25 14:16:56 +0530

    added alert function to textform.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 // import About from './Components/About';
+import Alert from './Components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState('null');
+  // const showAlert = (message, type) => {
+  //   setAlert({
+  //     msg: message,
+  //     type: type
+  //   })
+  //   setTimeout(() => {
+  //     setAlert(null);
+  //   }, 4500);
+  // }
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor = 'grey';
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
     }
+
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
 
   return (
     <>
       <Navbar title="TextUtiles" aboutText="about TextUtiles" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode} />
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
         {/* <About/> */}
       </div>
     </>
