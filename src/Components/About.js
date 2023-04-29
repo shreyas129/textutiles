282825b91f8901ebf1cc3a4d206426da32664fed
shreyas129 <shreyas.tathode129@gmail.com>
2023-04-29 15:52:03 +0530

    mode+

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,25 +3,30 @@ import { useState } from 'react'
 
 export default function About(props) {
 
-    const [myStyle, setMyStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white'
-    })
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // })
+
+    let myStyle = {
+        color: props.mode === 'dark' ? 'white' : '#042743',
+        backgroundColor: props.mode === 'dark' ? '#042743' : 'white',
+    }
 
     const [btntext, setBtnText] = useState("Enable dark mode")
 
     const toggleStyle = () => {
-        if(myStyle.color === 'black'){
-            setMyStyle({
-                    color: 'white',
-                    backgroundColor: 'black',
-                    border: '1px solid white'
+        if (myStyle.color === 'black') {
+            myStyle({
+                color: 'white',
+                backgroundColor: 'black',
+                border: '1px solid white'
             })
             setBtnText("Enable light mode")
             props.showAlert("Enabled dark mode!", "success");
         }
         else {
-            setMyStyle({
+            myStyle({
                 color: 'black',
                 backgroundColor: 'white'
             })
@@ -42,31 +47,34 @@ export default function About(props) {
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={myStyle}>
-                            <strong>This is used to correct the text.</strong> Such as moving to uppercase, moving to lowercase, removing the extra spaces, clearing, copying
+                            <strong>This is used to correct the text.</strong> Such as moving to uppercase, moving to lowercase, removing the extra spaces, clearing, copying, the text.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
                         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Accordion Item #2
+                            Free to use
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={myStyle}>
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <strong>This is free to use.</strong> This is free for editing the text. This is free for editing the text! So use it happily.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Accordion Item #3
+                            The dark mode
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={myStyle}>
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <strong>There are two types of dark mode in this application.</strong>There are two types of dark mode -
+                            <strong>1.</strong>Dark mode
+                            <strong>2.</strong>Dark mode +
+                            The 'Dark mode' is default dark mode and the 'Dark mode +' is expert dark mode that is in the bottom, So click it the you will see quiet neat Enjoy it
                         </div>
                     </div>
                 </div>
